2a63d185983cfe5c2cc1bff061f8122e33a5b882
php123456 <a_php@mail.ru>
2019-11-05 08:58:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+
+	$("#content").on("click", ".click1", function()
+	{
+        var arr = ["", "", ""];
+
+        arr = $(this).attr("id").split('_');
+        to_page(arr[1], arr[2]);
+
+	});
+
+	$("#content").on("click", "a", function(e)
+	{		e.preventDefault();
+
+        var arr = ["", "", ""];
+
+        arr = $(this).attr("id").split('_');
+
+		to_page(arr[1], arr[2]);
+
+		return false;
+	});
+
+	function to_page(page, id)
+	{ 		$.post("index.php", {"page":page, "id":id},
+   				function(data) {
+          		$("#content").html(data);
+   		});
+	}
+
+
+})
